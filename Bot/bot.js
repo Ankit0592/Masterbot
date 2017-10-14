2b6370a6a35d79beb8fdd47ae6091668ebf75fae
Ankit0592 <arora0592ankit@gmail.com>
2017-10-14 16:32:24 -0400

    usecase-1 initial commit

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,5 +1,6 @@
 
 var Botkit = require('botkit');
+var issue_id = 0;
 
 var controller = Botkit.slackbot({
   debug: false
@@ -15,3 +16,29 @@ controller.hears('Greetings',['mention', 'direct_mention','direct_message'], fun
   bot.reply(message,"Hello");
 
 });
+
+controller.hears('create',['mention', 'direct_mention','direct_message'], function(bot,message) 
+{
+  project_id = message.text.split(' ');
+
+  // var issue0 = nock("https://jira.atlassian.com")
+  // .post("/rest/api/2/issue")
+  // .reply(200, JSON.stringify({
+  //   "id": ++issue_id,
+  //   "key": "TST-24",
+  //   "self": "http://www.example.com/jira/rest/api/2/issue/"+issue_id
+  // }) );
+
+  var data = createIssueMock();
+
+  bot.reply(message,"Successfully created issue and issue id is: "+data.id);
+});
+
+
+function createIssueMock(){
+  return {
+    "id": ++issue_id,
+    "key": "TST-24",
+    "self": "http://www.example.com/jira/rest/api/2/issue/"+issue_id
+  }
+}
