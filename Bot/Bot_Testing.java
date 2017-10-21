b0f0c42446c0f1821fcae92f9d7a5a5171234995
sbiswas4 <sbiswas4@ncsu.edu>
2017-10-21 14:18:46 -0400

    Add files via upload

---
+++ Bot/Bot_Testing.java
@@ -0,0 +1,43 @@
+package selenium.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+public class WebTest
+{
+	private static WebDriver driver;
+	
+	@BeforeClass
+	public static void setUp() throws Exception 
+	{
+		//driver = new HtmlUnitDriver();
+		ChromeDriverManager.getInstance().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@AfterClass
+	public static void  tearDown() throws Exception
+	{
+		driver.close();
+		driver.quit();
+	} 
+	
+	
+
+}
