4f7ae79a83b0a828e48f40d9c9ca71869585dd12
SmokingSadhus <amedhek@ncsu.edu>
2017-10-25 14:58:25 -0400

    Selenium without Thread.sleep

--- Selenium/src/test/java/selenium/tests/WebTest.java
+++ Selenium/src/test/java/selenium/tests/WebTest.java
@@ -30,6 +30,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -39,7 +40,7 @@ import io.github.bonigarcia.wdm.ChromeDriverManager;
 public class WebTest
 {
 	private static WebDriver driver;
-	private String bot_name = "weather_name";
+	private String bot_name = "simissuebot";
 	private static WebDriverWait wait;
 	
 	@BeforeClass
@@ -225,7 +226,12 @@ public class WebTest
 		actions.sendKeys("Duplicate [10]");
 		actions.sendKeys(Keys.RETURN);
 		actions.build().perform();
-		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		//String query= "token=xoxb-260819033494-edR6l1FRV6NNbxKxwu0ZC7AN&channel=@weather_name&text=Hellooo";
 		
 		//String path=  "/api/chat.postMessage?"+ query;
@@ -236,8 +242,8 @@ public class WebTest
 				By.xpath("//span[@class='message_body' and text() = 'Found following duplicate issues']"));
 		assertNotNull(msg);
 	}
-	
-	@Test // Happy Path for create
+
+	@Test // Happy Path for duplicate
 	public void create() throws FileNotFoundException, IOException, ParseException
 	{	
 		// Switch to #selenium-bot channel and wait for it to load.
@@ -250,32 +256,136 @@ public class WebTest
 		Actions actions = new Actions(driver);
 		actions.moveToElement(messageBot);
 		actions.click();
+		
+		
 		actions.sendKeys("Create 500");
 		actions.sendKeys(Keys.RETURN);
 		actions.build().perform();
-		
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'Please enter issue type?']")));
-		//Object o= By.className("message_body");
+		List<WebElement> c1 = driver.findElements(By.xpath("//span[.='Create 500']/../.."));
+		WebElement lastElement = c1.get(c1.size()-1);
+		String lastElementId = lastElement.getAttribute("id");
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[.='Software Changes Survey']/../../div/div/span/img[@src = '/media/amazongc-micro.jpg']")));
+		//1) Bug(B) 2) Task(T) 3) Exit(E)
+		wait.until(new PageLoaded(lastElementId, "Please enter issue type? 1) Bug(B) 2) Task(T) 3) Exit(E)",false));
+		//List<WebElement> c2 = driver.findElements(By.xpath("//ts-message[@id = '"+lastElementId+"']"));
+		//System.out.println("Hi");
+		/*
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		*/
+		//List<WebElement> c3 = driver.findElements(By.xpath("//span[.='Please enter issue type?']"));
+		//List<WebElement> c1 = driver.findElements(By.xpath("//ts-message"));
+		//Issue 5143: Abhinav
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class,'message_body') and text() = 'Please enter issue type?']")));
+		//Object o= By.className("message_body");
 		actions.sendKeys("B");
 		actions.sendKeys(Keys.RETURN);
 		actions.build().perform();
-		
+		wait.until(new PageLoaded(lastElementId, "Please provide summary",false));
+		/*
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+/*"ok, I found these issues similar to one you are creating. Click on create against most relevant issue:
+
+	
+	"*/
 		actions.sendKeys("Description for this defect");
 		actions.sendKeys(Keys.RETURN);
 		actions.build().perform();
-		
-		/*wait.withTimeout(100, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+		wait.until(new PageLoaded(lastElementId, "Issue 5143: Abhinav",true));
+		/*
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
 
+		List<WebElement> btns = driver.findElements(By.xpath("//button[@title='Issue 5143: Abhinav']"));
+		WebElement btn = btns.get(btns.size() - 1);
+		btn.click();
+		wait.until(new PageLoaded(lastElementId, "Issue created and successfully assigned to : Abhinav",false));
+		/*try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		/*wait.withTimeout(100, TimeUnit.SECONDS).ignoring(StaleElementReferenceException.class);
+        
 		WebElement msg = driver.findElement(
 				By.xpath("//span[@class='msg_inline_attachment_row attachment_flush_text' and text() = 'Click the most relevant user']"));
 		assertNotNull(msg);*/
 	}
-
+	
+	@Test
+	public void createSad() throws FileNotFoundException, IOException, ParseException{
+		driver.get("https://se-projecthq.slack.com/messages/"+ bot_name);
+		wait.until(ExpectedConditions.titleContains(bot_name)); 
+		
+		
+		WebElement messageBot = driver.findElement(By.id("msg_input"));
+		assertNotNull(messageBot);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(messageBot);
+		actions.click();
+		
+		
+		actions.sendKeys("Create 500");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		List<WebElement> c1 = driver.findElements(By.xpath("//span[.='Create 500']/../.."));
+		WebElement lastElement = c1.get(c1.size()-1);
+		String lastElementId = lastElement.getAttribute("id");
+		wait.until(new PageLoaded(lastElementId, "Please enter issue type? 1) Bug(B) 2) Task(T) 3) Exit(E)",false));
+		actions.sendKeys("E");
+		actions.sendKeys(Keys.RETURN);
+		actions.build().perform();
+		wait.until(new PageLoaded(lastElementId, "Thanks for talking to me",false));
+	}
 }
+
+
+class PageLoaded implements ExpectedCondition<Boolean> {		
+	  String lastElementId;
+	  String message;
+	  Boolean isButton;
+		
+	  public PageLoaded(String lastElementId, String message, Boolean isButton) {
+	    this.lastElementId = lastElementId;	
+	    this.message = message;
+	    this.isButton = isButton;
+	   
+	  }
+
+	  public Boolean apply(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		WebDriver driver = (WebDriver)arg0;
+		//List<WebElement> c1 = driver.findElements(By.xpath("//span[.='Create 500']/../.."));
+		//List<WebElement> c2 = driver.findElements(By.xpath("//span[.='Please enter issue type?']"));
+		List<WebElement> c2;
+		if(isButton){
+			 c2 = driver.findElements(By.xpath("//button[@title='"+message+"']/../../../../../../../../.."));
+		}
+		else{
+		 c2 = driver.findElements(By.xpath("//span[.='"+message+"']/../.."));
+		}
+		if(c2.size()==0){
+			return false;
+		}
+		WebElement latest = c2.get(c2.size()-1);
+		int ret = latest.getAttribute("id").compareTo(lastElementId) ;
+		return ret >= 1;
+		
+		
+	}
+	}
