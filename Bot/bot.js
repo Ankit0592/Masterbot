71c1c216e128eeb26c08e4450abd09a9d0ff04e7
Pavneet Singh Anand <panand4@ncsu.edu>
2017-11-06 21:55:58 -0400

    Reverting bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -189,11 +189,11 @@ function getLikelyUsers(message){
 
 // fetch mock data for duplicate issues in use case-3
 function matchIssue(id,bot,message) {
-//  var mocked=nock("https://jira.atlassian.com/rest/api/2")
-//  .get("/search?jql=project=123")
-//  .reply(200, mockData["matching_issues"]);
-  var data = service.getIssues();
-//  var data=JSON.parse(mocked.interceptors[0].body);
+  var mocked=nock("https://jira.atlassian.com/rest/api/2")
+  .get("/search?jql=project=123")
+  .reply(200, mockData["matching_issues"]);
+//  var data = service.getIssues();
+  var data=JSON.parse(mocked.interceptors[0].body);
   var result = '';
   for(var i =0; i<data.length; i++){
     if (data[i].id === id) {
