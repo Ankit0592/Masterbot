dd9ba542b2e4b64009481d41194d267c112a6c89
Ankit0592 <arora0592ankit@gmail.com>
2017-11-09 17:43:04 -0500

    usecase-2 config variables

---
+++ service/config.js
@@ -0,0 +1,12 @@
+module.exports =
+{
+    team_members:[
+        
+        {   apshukla: process.env.apshukla,
+            aarora6: process.env.aarora6,
+            sbiswas4: process.env.sbiswas4,
+            amedhek: process.env.amedhek,
+            panand4: process.env.panand4
+        }
+    ]
+} 
